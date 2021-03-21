9c68a110090bb483cffc48e500934b0f5bb54924
hasanmahedi2511 <hasanmahedi2511@gmail.com>
2021-03-21 23:55:27 +0600

    Task done

---
+++ problems/problem 5.php
@@ -0,0 +1,4 @@
+<img src="aiub_logo.jpg" width="<?php echo($imageWidth)?>" height="<?php echo($imageHeight)?>" />
+<h2>American International University -<span style="background-color:<?php echo($backColor)?>;color:<?php echo($fontColor)?>">
+Bangladesh</h2>
+</span>
